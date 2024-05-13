590385e6028a7fe3b528d0f5efe05772119af24c
Mitali Patil <m.s.patil@student.tudelft.nl>
2024-05-13 18:54:31 +0200

    serve model code refactor

--- src/serve_model.py
+++ src/serve_model.py
@@ -12,42 +12,11 @@ swagger = Swagger(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    """
-    Predict whether a link is a phishing link.
-    ---
-    consumes:
-      - application/json
-    parameters:
-        - name: input_data
-          in: body
-          description: link to be evaluated.
-          required: True
-          schema:
-            type: object
-            required: link
-            properties:
-                link:
-                    type: string
-                    example: https://www.tudelft.nl/en/student/administration/termination-of-enrolment
-    responses:
-      200:
-        description: "The result of the classification: 'phishing' or 'legitimate'."
-    """
-    char_index = {
-        'a': 1,
-        'b': 2,
-        'c': 3,
-        'd': 4,
-        'e': 5,
-        'f': 6,
-        'g': 7,
-        'h': 8,
-        'i': 9,
-    }
-    tokenizer = libml.TokenizeQuery(char_index)
+
+    model = joblib.load('../model/release.joblib')
+    tokenizer = libml.TokenizeQuery(model["char_index"])
     query = request.get_json().get('link')
     processed_query = tokenizer.tokenize(query, 200)
-    model = joblib.load('../model/release.joblib')
     prediction = model.predict(processed_query)[0]
     prediction = (np.array(prediction) > 0.5).astype(int).tolist()  # 0 if phishing, 1 if legitimate
     print(prediction)
@@ -57,6 +26,8 @@ def predict():
         "Prediction": prediction
     }
     return jsonify(res)
+
+def tokenise():
 
 
 if __name__ == '__main__':
